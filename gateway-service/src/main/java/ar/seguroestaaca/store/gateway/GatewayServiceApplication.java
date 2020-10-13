e8f7d9e3b161aaf3e5ab606793c3780e717a371e
marcos.aguirre <marcos.aguirre86@outlook.com>
2020-10-13 14:44:35 -0300

    Commit incial Gateway API

---
+++ gateway-service/src/main/java/ar/seguroestaaca/store/gateway/GatewayServiceApplication.java
@@ -0,0 +1,16 @@
+package ar.seguroestaaca.store.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
---
+++ gateway-service/src/test/java/ar/seguroestaaca/store/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.seguroestaaca.store.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
